cce2d3e20a077563ca2ef6aeabe2fb6fa8b154ba
Evelien <edewals2009@gmail.com>
2026-06-30 16:51:00 +0200

    started adding 3d audio to the level

---
+++ Assets/Scripts/RandomAudio.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using UnityEngine;
+
+public class RandomAudio : MonoBehaviour
+{
+    [SerializeField] private AudioClip[] audioClips;
+    [SerializeField] private AudioSource AudioSource;
+
+    [SerializeField] private float minWait = 2;
+    [SerializeField] private float maxWait = 5;
+
+    private void Start()
+    {
+        StartCoroutine(playAudio());
+    }
+    private IEnumerator playAudio()
+    {
+        float randomWait = Random.Range(minWait, maxWait);
+        int audioClipToPlay = Random.Range(0, audioClips.Length);
+
+        AudioSource.PlayOneShot(audioClips[audioClipToPlay]);
+        yield return new WaitForSeconds(randomWait);
+        StartCoroutine(playAudio());
+    }
+}
