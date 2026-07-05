2b13318b276412358b2a790120f31fee6b9ea858
Evelien <edewals2009@gmail.com>
2026-07-05 16:39:54 +0200

    put in the final changes to be able to record

--- Assets/Scripts/RandomAudio.cs
+++ Assets/Scripts/RandomAudio.cs
@@ -9,7 +9,11 @@ public class RandomAudio : MonoBehaviour
     [SerializeField] private float minWait = 2;
     [SerializeField] private float maxWait = 5;
 
-    private void Start()
+    //private void Start()
+    //{
+    //    StartCoroutine(playAudio());
+    //}
+    private void OnEnable()
     {
         StartCoroutine(playAudio());
     }
@@ -17,7 +21,6 @@ public class RandomAudio : MonoBehaviour
     {
         float randomWait = Random.Range(minWait, maxWait);
         int audioClipToPlay = Random.Range(0, audioClips.Length);
-
         AudioSource.PlayOneShot(audioClips[audioClipToPlay]);
         yield return new WaitForSeconds(randomWait);
         StartCoroutine(playAudio());
